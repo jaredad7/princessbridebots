2af55d852637736c3b549f91cb76686f2bdabe49
Jared Dembrun <jared@debian.jared>
2018-12-08 11:09:43 -0500

    Working on /r/popular

--- justparrotsvizzini/vizzini.py
+++ justparrotsvizzini/vizzini.py
@@ -3,18 +3,16 @@ from vizzinilines import lines
 
 starttime = calendar.timegm(time.gmtime())
 
-reddit = praw.Reddit(user_agent='JustParrotsVizzini v0.1',
-                  client_id='IxSfcfeoTZ6smw',
-                  client_secret='IyvhSuN8SbehN9kEBtVxIz-_Bbg',
-                  username='JustParrotsVizzini',
-                  password='')
+botname = "vizzini"
+reddit = praw.Reddit(botname, user_agent='justparrotsvizzini')
+subreddit = reddit.subreddit('popular')
 
-subreddit = reddit.subreddit('princessbridebots')
-
+print("Ready to begin.")
 for comment in subreddit.stream.comments():
 	if comment.author != reddit.user.me() and starttime <= comment.created_utc:
+		print("New comment found at " + str(calendar.timegm(time.gmtime())))
 		for key in lines.keys():
 			if key in comment.body.lower() and starttime <= comment.created_utc:
-				print("New comment found! Responding...")
-				comment.reply(lines[key])
+				print("Responding...")
+				comment.reply(">" + key + "\n\n" + lines[key])
 		
